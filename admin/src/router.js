f2676d73da88a2eab6e2ed98a4add73ad167c920
liuxianghua1 <3374312139@qq.com>
2019-08-18 22:48:47 +0800

    使用通用Curd接口 实现物品Crud功能

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,9 @@ import Main from './views/Main.vue'
 import CategoryEdit from './views/CategoryEdit.vue'
 import CategoryList from './views/CategoryList.vue'
 
+import ItemEdit from './views/ItemEdit.vue'
+import ItemList from './views/ItemList.vue'
+
 
 Vue.use(Router)
 
@@ -14,6 +17,7 @@ export default new Router({
       name: 'Main',
       component: Main,
       children: [
+
         {
           path: '/categories/create',
           component: CategoryEdit
@@ -26,6 +30,20 @@ export default new Router({
         {
           path: '/categories/list',
           component: CategoryList
+        },
+
+        {
+          path: '/items/create',
+          component: ItemEdit
+        },
+        {
+          path: '/items/edit/:id',
+          component: ItemEdit,
+          props: true
+        },
+        {
+          path: '/items/list',
+          component: ItemList
         },
       ]
     },
