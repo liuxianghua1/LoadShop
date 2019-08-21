489caff9aaf6b6b629ac98714c4a7214112fc3b0
liuxianghua1 <3374312139@qq.com>
2019-08-21 10:54:46 +0800

    导航菜单

--- web/src/main.js
+++ web/src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import './style.scss';
+import router from './router'
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
