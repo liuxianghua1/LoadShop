3f03b7d9084211a1e4efdc6a1685aa06e108a04a
liuxianghua1 <3374312139@qq.com>
2019-08-21 12:21:04 +0800

    轮播图及美化

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
-    首页
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/54be0dc89324bc002fb52ce668338ab2.jpeg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/63363be7b59c38154c0f48671d9169fd.jpeg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/d0d82f2c7b4e0d6e4006187a87af2601.jpeg" />
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        }
+      }
+    };
+  }
+};
 </script>
-<style>
+<style lang="scss">
+@import '../assets/scss/variables';
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+      background: map-get($colors, 'white');
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, 'info');
+    }
+  }
+}
 </style>
